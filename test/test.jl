606463a181d7034c11024e8ba24b595a15284d08
Daniel Høegh <dhoegh91@gmail.com>
2015-12-05 11:11:47 +0100

    Use a module in the test script.

--- test/test.jl
+++ test/test.jl
@@ -1,3 +1,7 @@
+import DataFrames # Even if using was used here full qualification of the functions in Base.Test and DataFrames is needed
+import Base.Test
 function main()
+    df = DataFrames.@data([3, 2, 5, 4])
+    Base.Test.@test  maximum(df) == 5
     println("It works")
 end
