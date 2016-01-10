051910de13299362f653ddc03ed085f1879deae0
Daniel Høegh <dhoegh91@gmail.com>
2016-01-10 16:42:43 +0100

    Add patchelf to build script.

--- deps/build.jl
+++ deps/build.jl
@@ -2,3 +2,14 @@
     using WinRPM
     WinRPM.install("gcc",yes=true)
 end
+
+@linux_only begin
+    using BinDeps
+    run(@build_steps begin
+        `sudo add-apt-repository ppa:staticfloat/julia-deps -y`
+        `sudo apt-get update -qq -y`
+        `sudo apt-get install patchelf`
+    end)
+end
+
+    
