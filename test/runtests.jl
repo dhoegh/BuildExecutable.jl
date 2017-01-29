2e9eb3fe56e2823b4360f265c537cc787dd3a52f
Daniel Høegh <dhoegh91@gmail.com>
2017-01-29 16:14:00 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ try
     @test build_executable(exename, script, targetdir, "native"; force=false) == 0
     mv(targetdir, targetdir*"1")
     targetdir = targetdir*"1"
-    out = readstring(`$(joinpath(targetdir,exename * (is_winows() ? ".exe" : ""))) -arg1 arg2 -arg3 arg4`)
+    out = readstring(`$(joinpath(targetdir,exename * (is_windows() ? ".exe" : ""))) -arg1 arg2 -arg3 arg4`)
     @test contains(out, "-arg1,arg2,-arg3,arg4")
     @test contains(out, "It works")
 finally
